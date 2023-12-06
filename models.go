03c6f87edfc8241834f94997b4f634d0ae6fd078
GL-LuisGuzmán <luis.guzman@gorillalogic.com>
2023-12-05 22:13:50 -0500

    create feeds endpoint

--- models.go
+++ models.go
@@ -15,6 +15,15 @@ type User struct {
 	APIKey    string    `json:"api_key"`
 }
 
+type Feed struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Name      string    `json:"name"`
+	Url       string    `json:"url"`
+	UserId    uuid.UUID `json:"user_id"`
+}
+
 func DBUserToUser(Dbuser database.User) User {
 	return User{
 		ID:        Dbuser.ID,
@@ -22,5 +31,16 @@ func DBUserToUser(Dbuser database.User) User {
 		UpdatedAt: Dbuser.UpdatedAt,
 		Name:      Dbuser.Name,
 		APIKey:    Dbuser.ApiKey,
+	}
+}
+
+func DBFeedToFeed(DbFeed database.Feed) Feed {
+	return Feed{
+		ID:        DbFeed.ID,
+		CreatedAt: DbFeed.CreatedAt,
+		UpdatedAt: DbFeed.UpdatedAt,
+		Name:      DbFeed.Name,
+		Url:       DbFeed.Url,
+		UserId:    DbFeed.UserID,
 	}
 }
